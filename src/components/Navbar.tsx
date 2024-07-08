b6854a6d0bcfc97162feebb8c7702c3d85874a6d
GweepCreative <gweepcreative@gmail.com>
2024-07-08 21:52:31 +0300

    Frontend part1

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='bg-[#121212] w-full h-16 items-center justify-start flex p-4'>
+      <h1 className='text-white font-bold text-2xl'>
+        TeamVLR
+      </h1>
+    </div>
+  )
+}
