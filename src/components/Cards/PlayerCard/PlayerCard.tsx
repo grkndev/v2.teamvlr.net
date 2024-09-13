980324565f4bd3e0b747632c3a6002cbf5c61223
GweepCreative <gweepcreative@gmail.com>
2024-09-13 22:20:29 +0300

    build fix

--- src/components/Cards/PlayerCard/PlayerCard.tsx
+++ src/components/Cards/PlayerCard/PlayerCard.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
+import Image from "next/image";
 export default function PlayerCard({ player, deletePlayer }: { player: any, deletePlayer: (player: any) => void }) {
   return (
     <Card className=" bg-[#1E1E1E] w-[10rem] md:w-[12.5rem] border-2 border-[#2E2D2D] ">
@@ -14,7 +15,7 @@ export default function PlayerCard({ player, deletePlayer }: { player: any, dele
         <Trash size={16} />
       </Button>
       <div className="pt-4 px-4 flex items-start justify-start h-full w-full ">
-        <img className="w-full" src={player.photo_url} />
+        <Image className="w-full" src={player.photo_url} alt={player.name} width={200} height={200} />
       </div>
       <div className="bg-[#0A0A0A] w-full p-2 items-center flex justify-center rounded">
         <p className="text-white font-semibold">{player.name}</p>
--- src/components/Cards/SubPlayerCard/SubPlayerCard.tsx
+++ src/components/Cards/SubPlayerCard/SubPlayerCard.tsx
@@ -1,9 +1,10 @@
 import { Card } from "@/components/ui/card";
+import Image from "next/image";
 export default function SubPlayerCard({ player }: { player: any }) {
   return (
     <Card className="bg-[#1E1E1E]  w-[6rem] md:w-[9rem] border-2 border-[#2E2D2D] rounded-none">
       <div className="pt-4 px-4 flex items-start justify-start h-full w-full ">
-        <img className="w-full" src={player.photo_url} />
+        <Image className="w-full" src={player.photo_url} alt={player.name} width={200} height={200} />
       </div>
       <div className="bg-[#0A0A0A] w-full p-2 items-center flex justify-center">
         <p className="text-white font-semibold">{player.name}</p>
